9b45aa746939f2c4ec565b11891ba9a9a43e1905
istarlet <istarlet@gmail.com>
2022-03-11 16:11:10 -0500

    Update code

--- server.js
+++ server.js
@@ -8,39 +8,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect(
   "mongodb+srv://admin:dojgDsTQw7bnOS79@cluster0.ilbn4.mongodb.net/formDB",
   {
-    useNewUrlParser: true
+    useNewUrlParser: true,
   },
   { useUnifiedTopology: true }
 );
 
-//create a data schema
-
-const formSchema = {
-  name: String,
-  email: String
-};
-
-const Form = mongoose.model("Form", formSchema);
-
+//serve static files
 app.use(express.static(__dirname));
 
 app.use(express.static("src"));
 
 app.use(express.static("images"));
 
+
+//create a data schema
+const formSchema = {
+  name: String,
+  email: String,
+};
+
+const Form = mongoose.model("Form", formSchema);
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
+  
   let newForm = new Form({
     name: req.body.title,
-    email: req.body.content
+    email: req.body.content,
   });
   newForm.save();
   res.redirect("/");
 });
 
+//port listener
 app.listen(3000, function () {
   console.log("Server is running on 3000");
 });
