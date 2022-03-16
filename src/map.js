94613a7ca5a5119ec47873fc20dce22c2eda6bcd
istarlet <istarlet@gmail.com>
2022-03-15 23:36:30 -0400

    Update location section

--- src/map.js
+++ src/map.js
@@ -1,8 +1,9 @@
-// Making a map and tiles
+// Making a map
 const map = L.map("mapLouisville").setView([38.25817, -85.77126], 17);
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreet Map</a> contributors';
 
+// Making tiles  
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(map);
