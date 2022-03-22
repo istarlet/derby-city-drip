427ae6ba38fcb24dac9a1ad996d30119b6d5062b
istarlet <istarlet@gmail.com>
2022-03-22 00:39:12 -0400

    Update

--- src/form.js
+++ src/form.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form");
 const log = document.getElementById('log');
-form.addEventListener('submit', logSubmit);
+
 
 form.addEventListener('submit', function(e) {
     log.textContent = "Form Submitted!";
