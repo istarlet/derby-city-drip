791db31d26d45c8f79437c560e8dd6910cc75a9f
istarlet <istarlet@gmail.com>
2022-03-23 17:17:37 -0400

    Trying to get form success message to work

--- src/form.js
+++ src/form.js
@@ -2,7 +2,7 @@ const form = document.getElementById("form");
 const log = document.getElementById("log");
 
 form.addEventListener("submit", (e) => {
-  log.textContent = "Form Submitted!";
+  log.textContent = "Form submitted successfully!";
   form.reset();
-  e.preventDefault();
+  //e.preventDefault();
 });
