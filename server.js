bb69e20438628b974e9f3d56784bdc2ea60afc4a
istarlet <istarlet@gmail.com>
2022-03-26 01:35:06 -0400

    Playing around with form

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const res = require("express/lib/response");
 const { response } = require("express");
+var cors = require("cors");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(
   "mongodb+srv://admin:dojgDsTQw7bnOS79@cluster0.ilbn4.mongodb.net/formDB",
@@ -35,14 +39,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function (req, res) {
+app.post("/dcdform", function (req, res) {
   const newForm = new Form({
     name: req.body.name,
     email: req.body.email,
   });
 
   newForm.save();
-  res.redirect("/");
 });
 
 //port listener
