35a273982640ba3f6c26eb6edf12196af71f4724
istarlet <istarlet@gmail.com>
2022-04-07 01:57:30 -0400

    Thought I broke my code. Forgot to run on 3000

--- src/formtest.js
+++ src/formtest.js
@@ -1,5 +1,8 @@
-const successMsg = document.getElementById("button");
+let successMsg = document.getElementById("button");
 const dcdForm = document.getElementById("dcdform");
+
+// Hold the timeout
+let timeout;
 
 async function postFormDataAsJson({ url, formData }) {
   const plainFormData = Object.fromEntries(formData.entries());
@@ -15,9 +18,22 @@ async function postFormDataAsJson({ url, formData }) {
   };
 
   const response = await fetch(url, fetchOptions);
+
   // This is where I add things once form is submitted
-  successMsg.textContent = "Form Submitted!";
-  dcdForm.reset();
+
+  successMsg.classList.add("spin");
+  successMsg.disabled = true;
+
+  // successMsg.textContent = "Form is submitting";
+
+  clearTimeout(timeout);
+
+  timeout = setTimeout(function () {
+    successMsg.classList.remove("spin");
+
+    successMsg.textContent = "Form submission success!";
+    dcdForm.reset();
+  }, 4000);
 
   if (!response.ok) {
     const errorMessage = await response.text();
